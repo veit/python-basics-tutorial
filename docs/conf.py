c93d233a34612af35e481e940adcb77579787e6e
Veit Schiele <veit@cusy.io>
2024-04-11 16:07:20 +0200

    :wrench: Add pygments_pytest

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ import re
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    "pygments_pytest",
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
